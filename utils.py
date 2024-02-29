875f94da628fd9b0d1af0d158fe88a13346b0fe3
ShewchukI <158152890+ShewchukI@users.noreply.github.com>
2024-02-29 22:27:04 +0200

    file utils.py (4.2.2)

---
+++ utils.py
@@ -0,0 +1,14 @@
+def factorial(f):
+    if f == 0 or f == 1:
+        return 1
+    else:
+        return n * factorial(f - 1)
+
+
+def is_prime(n):
+    if n <= 1:
+        return False
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
